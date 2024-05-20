5d793795b66cca5c4c1d6c920ece77684c41a1fb
Michał Szwindowski <github@nedi.me>
2024-05-20 22:48:34 +0200

    make build

---
+++ index.9e33a464.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page--menu-visible"):document.body.classList.remove("page--menu-visible")}),document.querySelector(".contact-form__button").addEventListener("click",function(e){},!1);
+//# sourceMappingURL=index.9e33a464.js.map
